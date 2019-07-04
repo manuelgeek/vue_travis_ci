426d81393bc4c2c5e393abd56783be69b973337b
manuelgeek <emashmagak@appslab.co.ke>
2019-07-04 13:30:15 +0300

    added unit test to List.vue

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import List from './views/List.vue'
 
 Vue.use(Router)
 
@@ -20,6 +21,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    {
+        path: '/to-do',
+        name: 'ToDo',
+        component: List
+      },
   ]
 })
---
+++ tests/unit/List.spec.js
@@ -0,0 +1,30 @@
+import List from '@/views/List';
+import { expect } from 'chai'
+import Vue from 'vue';
+
+describe('List.vue', () => {
+  it('displays items from the list', () => {
+    const Constructor = Vue.extend(List);
+    const ListComponent = new Constructor().$mount();
+    expect(ListComponent.$el.textContent).to.contain('play games');
+  })
+
+  it('adds a new item to list on click', () => {
+    // build component
+    const Constructor = Vue.extend(List);
+    const ListComponent = new Constructor().$mount();
+
+    // set input value
+    ListComponent.newItem = 'brush my teeth';
+
+    // simulate click event
+    const button = ListComponent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    ListComponent._watcher.run();
+
+    // assert list contains new item
+    expect(ListComponent.$el.textContent).to.contain('brush my teeth');
+    expect(ListComponent.listItems).to.contain('brush my teeth');
+  })
+})
