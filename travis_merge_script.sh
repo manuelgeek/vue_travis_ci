d3ee12f75797fbaeaf77f0f7cdab6cb7acac6063
manuelgeek <emashmagak@appslab.co.ke>
2020-02-27 20:10:14 +0300

    added latest

--- travis_merge_script.sh
+++ travis_merge_script.sh
@@ -5,6 +5,7 @@ export GIT_COMMITTER_EMAIL="emashmagak@appslab.co.ke"
 export GIT_COMMITTER_NAME="ManuEl Geek"
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
+git stash
 git checkout master || exit
 git merge --no-ff "$TRAVIS_COMMIT" || exit
 git push https://"${GITHUB_TOKEN}"@github.com/manuelgeek/vue_travis_ci.git
